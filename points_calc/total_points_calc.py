bdd0d97042da24cea5731d8abec64131ceb0dde4
Madhavan <madhavan0123@gmail.com>
2026-02-09 23:21:03 -0800

    Code to update daily scores

--- points_calc/total_points_calc.py
+++ points_calc/total_points_calc.py
@@ -2,7 +2,7 @@ import os
 import glob
 import pandas as pd
 
-FOLDER = r"C:\Users\madha\Documents\Auctions_Code"  # change if needed
+FOLDER = r"C:\Users\madha\Documents\2026-T20-WC-Auction\points_calc"  # change if needed
 PLAYER_COL = "0"   # change if your column name is different
 POINTS_COL = "1"   # change if your column name is different
 
@@ -12,7 +12,8 @@ dfs = []
 for path in all_files:
     df = pd.read_csv(path)
     if PLAYER_COL in df.columns and POINTS_COL in df.columns:
-        dfs.append(df[[PLAYER_COL, POINTS_COL]])
+        temp = df[[PLAYER_COL, POINTS_COL]]
+        dfs.append(temp)
     else:
         print(f"Skipping {os.path.basename(path)} (missing columns)")
 
@@ -21,8 +22,15 @@ if not dfs:
 
 combined = pd.concat(dfs, ignore_index=True)
 
+combined[POINTS_COL] = combined[POINTS_COL].str.split(' ').str[0]
+combined[POINTS_COL] = pd.to_numeric(combined[POINTS_COL], errors='coerce').fillna(0).astype(int)
+combined[PLAYER_COL] = combined[PLAYER_COL].str.split('(').str[0]
+
 # sum points per player
 summary = combined.groupby(PLAYER_COL, as_index=False)[POINTS_COL].sum()
+
+out_path = os.path.join(FOLDER, "temp.csv")
+combined.to_csv(out_path, index=False)
 
 out_path = os.path.join(FOLDER, "players_total_points.csv")
 summary.to_csv(out_path, index=False)
